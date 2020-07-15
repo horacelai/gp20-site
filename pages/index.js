21e84591fe208b2363b6105160af8adc8c58dbf7
Ho Long Lai <horace@iMac.local>
2020-07-16 00:45:59 +0800

    add home page item

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import Nav from '../components/nav';
 export default function Home() {
   const assetPath = process.env.ASSET_PREFIX;
   return (
-    <div className="container">
+    <div className="home">
       <Head>
         <title>她他她的戀愛守則 | 科大劇社畢業匯演 2020</title>
         <link rel="icon" href={`${assetPath}favicon.ico`} />
       </Head>
-
+      {/* <div className="alert-bar noise">
+        <div>受疫情影響，演出日期可能會被延遲或取消，請留意劇社社交網站以取得最新資訊</div>
+      </div> */}
       <main>
         <Nav />
         <div className="landing noise">
@@ -31,6 +33,22 @@ export default function Home() {
       <section className="landing-quote noise">
         <div>
         「如果有個女仔走嚟同妳講佢鍾意妳呢？」
+        </div>
+      </section>
+
+      <section className="bottom noise">
+        <div className="container">
+          <h2>演出詳情</h2>
+          <div className="details">
+            <div><span className="title">日期</span> 8月29日 8月30日</div>
+            <div><span className="title">時間</span> 下午 8 時</div>
+            <div><span className="title">地點</span> 西灣河文娛中心</div>
+          </div>
+          <a href="http://facebook.com" target="_blank" rel="noopener noreferrer" className="call-to-action-btn">立即訂票</a>
+          <div className="remarks">
+            <p>不設劃位，座位先到先得</p>
+            <p>受疫情影響，演出日期可能會被延遲或取消，請留意劇社社交網站以取得最新資訊</p>
+          </div>
         </div>
       </section>
 
